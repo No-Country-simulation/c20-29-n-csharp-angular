1ea32821bc0be9783dda3ef9a7aa6ac39f42f54a
Juan Pacheco <jpachecoim044@gmail.com>
2024-08-30 21:30:40 -0500

    Borrador del product-form

--- insta-patitas-front/src/app/components/product-form/product-form.component.ts
+++ insta-patitas-front/src/app/components/product-form/product-form.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-product-form',
+  selector: 'product-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './product-form.component.html',
-  styleUrl: './product-form.component.css'
+  styleUrl: './product-form.component.css',
 })
-export class ProductFormComponent {
+export class ProductFormComponent implements OnInit {
+  productForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+
+      
+    });
+  }
 }
