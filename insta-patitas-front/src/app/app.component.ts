7f398026b09a972caf7c823fbbefefa03d30217a
felipehmora <felipehmoraarg@gmail.com>
2024-09-10 18:45:22 -0400

    shelter form & create-post

--- insta-patitas-front/src/app/app.component.ts
+++ insta-patitas-front/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ShelterFormComponent } from './components/shelter-form/shelter-form.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, ShelterFormComponent],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    ShelterFormComponent,
+    CreatePostComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- insta-patitas-front/src/app/app.routes.ts
+++ insta-patitas-front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ShelterFormComponent } from './components/shelter-form/shelter-form.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
 
 export const routes: Routes = [
   { path: 'shelter-form', component: ShelterFormComponent },
+  { path: 'create-post', component: CreatePostComponent },
 ];
