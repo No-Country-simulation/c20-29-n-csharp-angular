23a638e172677cfbf26fb10a417efe66cb00e2b4
Juan Pacheco <jpachecoim044@gmail.com>
2024-09-11 18:51:12 -0500

    push para realizar pull request

--- insta-patitas-front/src/app/app.routes.ts
+++ insta-patitas-front/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { authRouteGuard } from "./guards/auth-route.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "",
+		pathMatch: "full",
+		redirectTo: "patitas",
+	},
+	{
+		path: "patitas",
+		loadComponent: () =>
+			import("./pages/home/home.component").then((c) => c.HomeComponent),
+		canActivate: [authRouteGuard],
+	},
+];
