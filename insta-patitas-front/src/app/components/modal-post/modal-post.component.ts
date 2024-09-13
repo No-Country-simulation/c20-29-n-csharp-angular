671bbedf4cf86cbb7c199bd3f6b5ea88947b5283
Yusleidy Gamboa <129456753+Yusleidy20@users.noreply.github.com>
2024-09-12 20:25:38 -0500

    cambios en el modal-post

--- insta-patitas-front/src/app/components/modal-post/modal-post.component.ts
+++ insta-patitas-front/src/app/components/modal-post/modal-post.component.ts
@@ -9,13 +9,44 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./modal-post.component.css']
 })
 export class ModalComponent {
-  info: string | null = null;
+   // Arreglo para almacenar las opciones seleccionadas
+   info: string[] = []; 
 
-  showInfo(option: string) {
-    if (this.info === option) {
-      this.info = null; 
-    } else {
-      this.info = option;
-    }
-  }
-}
+   // Objeto para controlar la visibilidad de los mensajes de ayuda
+   helpVisible: { [key: string]: boolean } = {}; 
+ 
+   // Propiedad para determinar si la opción "Adoptando" está deshabilitada
+   get isAdoptandoDisabled(): boolean {
+     // Deshabilita "Adoptando" si "Donando" o "Apadrinando" están seleccionados
+     return this.info.includes('donando') || this.info.includes('apadrinando');
+   }
+ 
+   // Propiedad para determinar si la opción "Donando" está deshabilitada
+   get isDonandoDisabled(): boolean {
+     // Deshabilita "Donando" si "Adoptando" está seleccionado
+     return this.info.includes('adoptando');
+   }
+ 
+   // Propiedad para determinar si la opción "Apadrinando" está deshabilitada
+   get isApadrinandoDisabled(): boolean {
+     // Deshabilita "Apadrinando" si "Adoptando" está seleccionado
+     return this.info.includes('adoptando');
+   }
+ 
+   // Método para alternar la visibilidad de los mensajes de ayuda
+   showInfo(option: string) {
+     // Cambia la visibilidad del mensaje de ayuda para la opción dada
+     this.helpVisible[option] = !this.helpVisible[option];
+   }
+ 
+   // Método para manejar los cambios en los checkboxes
+   onCheckboxChange(option: string) {
+     // Si la opción ya está en la lista, la elimina (deselecciona)
+     if (this.info.includes(option)) {
+       this.info = this.info.filter(item => item !== option);
+     } else {
+       // Si la opción no está en la lista, la añade (selecciona)
+       this.info.push(option);
+     }
+   }
+ }
